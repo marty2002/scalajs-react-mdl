28f4013668f49a506a618d52e16a8aecc383ce29
Kovalenko Kirill <k.b.kovalenko@gmail.com>
2017-02-08 13:09:45 +0700

    add Buttons, FABButtons, IconButtons (#4)

--- examples/ex1/src/main/scala/Main.scala
+++ examples/ex1/src/main/scala/Main.scala
@@ -8,7 +8,8 @@ import eldis.react.ReactNode
 
 import scalajs.js
 import eldis.react._
-import vdom.prefix_<^._
+import eldis.react.mdl.components
+import vdom.prefix_<^.{ <, _ }
 import org.scalajs.dom
 import eldis.react.mdl.components._
 
@@ -19,7 +20,21 @@ object Main extends js.JSApp {
       <.h4()("The Icon:"),
       Icon(Icon.Props(name = "save", key = Some("save_icon"))),
       <.h4()("The checkbox: "),
-      Checkbox("This is checkbox")
+      Checkbox("This is checkbox"),
+      <.h4()("The buttons: "),
+      Button("Button"), Button("Button", Button.Props(raised = Some(true))),
+      <.h4()("The flat buttons: "),
+      FlatButton("Flat"), FlatButton("Flat", disabled = Some(true)),
+      <.h4()("The raised buttons: "),
+      RaisedButton("Raised"), RaisedButton("Raised", disabled = Some(true)),
+      <.h4()("The colored buttons: "),
+      ColoredButton("Colored"), ColoredButton("Colored", disabled = Some(true)),
+      <.h4()("The accent buttons: "),
+      AccentButton("Accent"), AccentButton("Accent", disabled = Some(true)),
+      <.h4()("The FABbuttons: "),
+      FABButton("add"), FABButton("add", FABButton.Props(colored = Some(true), mini = Some(true))),
+      <.h4()("The icon buttons: "),
+      IconButton("mood"), IconButton(IconButton.Props(icon = Some("mood"), colored = Some(true)))
     )
   }
 
---
+++ src/main/scala/components/Buttons.scala
@@ -0,0 +1,177 @@
+/**
+ * Created by kirill on 08.02.2017.
+ */
+package eldis.react.mdl.components
+
+import eldis.react._
+import eldis.react.mdl._
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+import js.JSConverters._
+
+object Button {
+  @js.native
+  trait Props extends ReactMDLCommonPropsJS {
+    val raised: js.UndefOr[Boolean] = js.native
+    val colored: js.UndefOr[Boolean] = js.native
+    val accent: js.UndefOr[Boolean] = js.native
+    val primary: js.UndefOr[Boolean] = js.native
+    val disabled: js.UndefOr[Boolean] = js.native
+    val onClick: js.UndefOr[js.Function0[Unit]] = js.native
+    val ripple: js.UndefOr[Boolean] = js.native
+    val title: js.UndefOr[String] = js.native
+  }
+
+  object Props {
+    def apply(
+      className: Seq[String] = Nil,
+      key: Option[String] = None,
+      style: Option[js.Object] = None,
+      raised: Option[Boolean] = None,
+      colored: Option[Boolean] = None,
+      accent: Option[Boolean] = None,
+      primary: Option[Boolean] = None,
+      disabled: Option[Boolean] = None,
+      onClick: Option[Unit => Unit] = None,
+      ripple: Option[Boolean] = None,
+      title: Option[String] = None
+    ) = js.Dynamic.literal(
+      className = fillClassAttr(className).orUndefined,
+      key = key.orUndefined,
+      style = style.orUndefined,
+      raised = raised.orUndefined,
+      colored = colored.orUndefined,
+      accent = accent.orUndefined,
+      primary = primary.orUndefined,
+      disabled = disabled.orUndefined,
+      onClick = onClick.orUndefined,
+      ripple = ripple.orUndefined,
+      title = title.orUndefined
+    ).asInstanceOf[Props]
+  }
+
+  @JSImport("react-mdl", "Button")
+  @js.native
+  object Component extends JSComponent[Props]
+
+  def apply(label: String, p: Props = Props()) = React.createElement(Component, p, label)
+}
+
+object FlatButton {
+  def apply(label: String, onClick: Option[Unit => Unit] = None, disabled: Option[Boolean] = None, title: Option[String] = None) =
+    Button.apply(label, Button.Props(raised = Some(false), disabled = disabled, onClick = onClick, title = title))
+}
+
+object RaisedButton {
+  def apply(label: String, onClick: Option[Unit => Unit] = None, disabled: Option[Boolean] = None, title: Option[String] = None) =
+    Button.apply(label, Button.Props(raised = Some(true), disabled = disabled, onClick = onClick, title = title))
+}
+
+object ColoredButton {
+  def apply(label: String, onClick: Option[Unit => Unit] = None, disabled: Option[Boolean] = None, title: Option[String] = None) =
+    Button.apply(label, Button.Props(colored = Some(true), disabled = disabled, onClick = onClick, title = title))
+}
+
+object AccentButton {
+  def apply(label: String, onClick: Option[Unit => Unit] = None, disabled: Option[Boolean] = None, title: Option[String] = None) =
+    Button.apply(label, Button.Props(accent = Some(true), disabled = disabled, onClick = onClick, title = title))
+}
+
+object FABButton {
+
+  @js.native
+  trait Props extends ReactMDLCommonPropsJS {
+    val colored: js.UndefOr[Boolean] = js.native
+    val ripple: js.UndefOr[Boolean] = js.native
+    val disabled: js.UndefOr[Boolean] = js.native
+    val mini: js.UndefOr[Boolean] = js.native
+    val onClick: js.UndefOr[js.Function0[Unit]] = js.native
+    val title: js.UndefOr[String] = js.native
+  }
+
+  object Props {
+    def apply(
+      className: Seq[String] = Nil,
+      key: Option[String] = None,
+      style: Option[js.Object] = None,
+      colored: Option[Boolean] = None,
+      ripple: Option[Boolean] = None,
+      disabled: Option[Boolean] = None,
+      mini: Option[Boolean] = None,
+      onClick: Option[Unit => Unit] = None,
+      title: Option[String] = None
+    ) =
+      js.Dynamic.literal(
+        className = fillClassAttr(className).orUndefined,
+        key = key.orUndefined,
+        style = style.orUndefined,
+        colored = colored.orUndefined,
+        ripple = ripple.orUndefined,
+        disabled = disabled.orUndefined,
+        mini = mini.orUndefined,
+        onClick = onClick.orUndefined,
+        title = title.orUndefined
+      ).asInstanceOf[Props]
+  }
+
+  @JSImport("react-mdl", "FABButton")
+  @js.native
+  object Component extends JSComponent[Props]
+
+  def apply(icon: String, p: Props) = React.createElement(Component, p, Icon(Icon.Props(name = icon)))
+  def apply(icon: String, onClick: Option[Unit => Unit] = None, disabled: Option[Boolean] = None, ripple: Option[Boolean] = Some(true), title: Option[String] = None) =
+    React.createElement(Component, Props(onClick = onClick, disabled = disabled, ripple = ripple, title = title), Icon(Icon.Props(name = icon)))
+}
+
+object IconButton {
+
+  @js.native
+  trait Props extends ReactMDLCommonPropsJS {
+    val name: js.UndefOr[String] = js.native
+    val colored: js.UndefOr[Boolean] = js.native
+    val ripple: js.UndefOr[Boolean] = js.native
+    val disabled: js.UndefOr[Boolean] = js.native
+    val accent: js.UndefOr[Boolean] = js.native
+    val primary: js.UndefOr[Boolean] = js.native
+    val onClick: js.UndefOr[js.Function0[Unit]] = js.native
+    val title: js.UndefOr[String] = js.native
+  }
+
+  object Props {
+    def apply(
+      className: Seq[String] = Nil,
+      key: Option[String] = None,
+      style: Option[js.Object] = None,
+      icon: Option[String] = None,
+      colored: Option[Boolean] = None,
+      ripple: Option[Boolean] = None,
+      disabled: Option[Boolean] = None,
+      accent: Option[Boolean] = None,
+      primary: Option[Boolean] = None,
+      onClick: Option[Unit => Unit] = None,
+      title: Option[String] = None
+    ) =
+      js.Dynamic.literal(
+        className = fillClassAttr(className).orUndefined,
+        key = key.orUndefined,
+        style = style.orUndefined,
+        name = icon.orUndefined,
+        colored = colored.orUndefined,
+        ripple = ripple.orUndefined,
+        disabled = disabled.orUndefined,
+        accent = accent.orUndefined,
+        primary = primary.orUndefined,
+        onClick = onClick.orUndefined,
+        title = title.orUndefined
+      ).asInstanceOf[Props]
+  }
+
+  @JSImport("react-mdl", "IconButton")
+  @js.native
+  object Component extends JSComponent[Props]
+
+  def apply(p: Props) = React.createElement(Component, p)
+  def apply(icon: String, onClick: Option[Unit => Unit] = None, disabled: Option[Boolean] = None, title: Option[String] = None) = React.createElement(Component, Props(icon = Some(icon), disabled = disabled, onClick = onClick, title = title))
+}
+
